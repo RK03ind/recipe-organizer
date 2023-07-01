e1b1bb06b4c717a1f011eee1b26abdd5f97c4d68
RK03ind <gamitech95@gmail.com>
2023-07-01 22:07:44 +0530

    idk anything commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { DataProvider } from "./Context/DataContext.jsx";
+import { DataProvider } from "./context/DataContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
